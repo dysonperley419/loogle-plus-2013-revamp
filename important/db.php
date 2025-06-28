a04d594ad22a36bb385f1f7b359cece26d6def24
dysonperley419 <perleydyson903@gmail.com>
2025-06-28 00:59:47 -0300

    Update db.php

--- important/db.php
+++ important/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "nicknick";
-$db_name = "loogle";
+$db_host = "mysql2.serv00.com";
+$db_user = "m11265_dexptuba3";
+$db_pass = "Dysonp2013";
+$db_name = "m11265_loogle";
 
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
     $protocol = 'https';  
@@ -15,10 +15,10 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 
 $host = $_SERVER['HTTP_HOST'] ?? '';
 
-if (strpos($host, 'localhost:8090') !== false) {
-    $siteurl = $protocol . "://localhost:8090";  
-} elseif (strpos($host, 'localhost:8090') !== false) {
-    $siteurl = $protocol . "://localhost:8090";  
+if (strpos($host, 'kspc.jawed2005.serv00.net') !== false) {
+    $siteurl = $protocol . "://kspc.jawed2005.serv00.net";  
+} elseif (strpos($host, 'kspc.jawed2005.serv00.net') !== false) {
+    $siteurl = $protocol . "://kspc.jawed2005.serv00.net";  
 } else {
 
     header('HTTP/1.0 400 Bad Request');
